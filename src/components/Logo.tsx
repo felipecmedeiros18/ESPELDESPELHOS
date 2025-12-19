6453ba74b09fb7b7908268982006c8d770558618
Felipe Medeiros <felipecmedeiros18@gmail.com>
2025-12-19 15:56:29 +0000

    aumente um pouco a logotipo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <a href="/" aria-label="ESPELD Home" className="flex items-center gap-3">
-      <Image src="https://i.imgur.com/l3z6Wu0.png" alt="ESPELD Logo" width={32} height={32} className="h-8 w-8" />
+      <Image src="https://i.imgur.com/l3z6Wu0.png" alt="ESPELD Logo" width={40} height={40} className="h-10 w-10" />
       <span
         className={cn(
           "font-headline text-3xl font-bold tracking-tighter text-primary",
