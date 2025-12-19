9f12c7c5ef150fd72074c3dc33c4dbbb007f4718
Felipe Medeiros <felipecmedeiros18@gmail.com>
2025-12-19 15:57:09 +0000

    mais um pouco grande

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <a href="/" aria-label="ESPELD Home" className="flex items-center gap-3">
-      <Image src="https://i.imgur.com/l3z6Wu0.png" alt="ESPELD Logo" width={40} height={40} className="h-10 w-10" />
+      <Image src="https://i.imgur.com/l3z6Wu0.png" alt="ESPELD Logo" width={48} height={48} className="h-12 w-12" />
       <span
         className={cn(
           "font-headline text-3xl font-bold tracking-tighter text-primary",
